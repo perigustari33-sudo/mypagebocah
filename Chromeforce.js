c0cad3a008415d21a81839d0ca4408037be3938f
perigustari <perigustari33@gmail.com>
2026-07-03 12:37:55 +0700

    Update Chromeforce.js

--- Chromeforce.js
+++ Chromeforce.js
@@ -1,8 +1,18 @@
-var uag = navigator.userAgent;
-var cekgau = uag.match(/samsungBrowser|wv|vivaldi|edge|opera|firefox/i);
-if (cekgau) {
-    var currentUrl = window.location.href;
-        var chromeIntent = 'intent://' + currentUrl.replace(/^https?:\/\//, '') + 
-                          '#Intent;scheme=https;package=com.android.chrome;end';        
-        window.location.href = chromeIntent;
-}
+(function() {
+    var ua = navigator.userAgent;
+    
+    // Cek Android dan bukan Chrome
+    var isAndroid = /Android/i.test(ua);
+    var isChrome = /Chrome/i.test(ua) && !/Edg|OPR|SamsungBrowser|Vivaldi|FBAN|FBAV|Instagram|wv/i.test(ua);
+    
+    // Cek apakah sudah di-redirect
+    var alreadyRedirected = window.location.search.includes('redirected=true');
+    
+    if (isAndroid && !isChrome && !alreadyRedirected) {
+        var url = window.location.href;
+        var redirectUrl = url + (url.includes('?') ? '&' : '?') + 'redirected=true';
+        var intent = 'intent://' + redirectUrl.replace(/^https?:\/\//, '') + 
+                    '#Intent;scheme=https;package=com.android.chrome;end';
+        window.location.href = intent;
+    }
+})();
